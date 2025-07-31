de754def5738d3f63be2245a0a781882f83011a7
Franz Sarmiento <hftfranz@gmail.com>
2025-08-01 01:01:42 +0800

    Created some functions to create a linked list, append to list, and free the linked list after its use.

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+
+typedef struct Node{
+    uint8_t data[64];
+    struct Node* next;
+} Node;
+
+Node* appendToList(uint8_t* data, Node* pHead){
+    Node * cursor = pHead;
+    while (cursor->next !=NULL){
+        cursor = cursor->next;
+    }
+    Node* newNode = (Node*) malloc(sizeof(Node));
+    memcpy(newNode->data, data, sizeof(data));
+    newNode->next = NULL;
+    cursor->next = newNode;
+    return pHead;
+}
+
+Node* createLinkedList(uint8_t* data){
+    Node* newHead = (Node*) malloc(sizeof(Node));
+    memcpy(newHead->data, data, sizeof(data));
+    newHead->next = NULL;
+    return newHead;
+}
+
+void freeLinkedList(Node* pHead){
+    Node* cursor = pHead;
+    Node* nextcursor = NULL;
+    while(cursor != NULL){
+        nextcursor = cursor->next;
+        free(cursor);
+        cursor = nextcursor;
+    }
+}
 
 int main()
 {
@@ -9,5 +44,6 @@ int main()
     printf("\nHello World and %s\n", name);
     strcpy(pName, "Franz");
     printf("New name is now %s\n", pName);
+    printf("Size of Node: %d", sizeof(Node));
     return 0;
 }
